082bd1ecb41c718fc4fc25a292c82d32b5ade117
tania <tanislavens@yahoo.com>
2020-01-17 17:50:35 -0500

    changed ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,35 +7,35 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    for weight in weights:
-        # index = len(weights)
-        index = 0
-        value = limit - weight
-        ht_val = hash_table_retrieve(ht, weight)
-
-        if weight != ht_val:
-            hash_table_insert(ht, weight, value)
-            index += 1
-        elif weight == ht_val:
-            unsorted_index = weights.index(ht_val)
-            answer = (index, unsorted_index)
-            return answer
-    return None
-
     # for weight in weights:
-    #     index = len(weights)
+    #     # index = len(weights)
     #     index = 0
-    #     difference = limit - weight
-    #     value = (index, difference)
+    #     value = limit - weight
     #     ht_val = hash_table_retrieve(ht, weight)
 
-    #     if weight != ht_val[1]:
+    #     if weight != ht_val:
     #         hash_table_insert(ht, weight, value)
     #         index += 1
-    #     elif weight == ht_val[1]:
-    #         return (weight[index], ht_val[0])
-    # else:
-    #     return None
+    #     elif weight == ht_val:
+    #         unsorted_index = weights.index(ht_val)
+    #         answer = (index, unsorted_index)
+    #         return answer
+    # return None
+
+    for weight in weights:
+        index = len(weights)
+        index = 0
+        difference = limit - weight
+        value = (index, difference)
+        ht_val = hash_table_retrieve(ht, weight)
+
+        if weight != ht_val[1]:
+            hash_table_insert(ht, weight, value)
+            index += 1
+        elif weight == ht_val[1]:
+            return (weight[index], ht_val[0])
+    else:
+        return None
 
 
 get_indices_of_item_weights([1, 2, 3, 4, 5], 5, 7)
