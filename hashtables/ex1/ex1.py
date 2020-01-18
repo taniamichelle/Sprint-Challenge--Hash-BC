67dc3630b601922609ee5036d60087eee1253678
tania <tanislavens@yahoo.com>
2020-01-18 08:40:35 -0500

    attempt to store ht value as a tuple

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -11,30 +11,40 @@ def get_indices_of_item_weights(weights, length, limit):
     for i in weights:
         # Define `weight` as current weight
         weight = weights[i]
-        # # Calculate limit-weight + store as `difference`
-        # difference = limit - weight
+        # Calculate limit-weight + store as `difference`
+        difference = limit - weight
         # Define `value` as a tuple: (curr index, difference)
-        # value = (i, difference)
-
-        print(f"weight: {weight}")
-
-        # If weight not in ht:
-        if weight not in ht:
-            # Add key:weight + value:(its_index, difference) to ht
-            hash_table_insert(ht, weight, i)
-        # If weight IS in ht:
-        else:
-            pass
-
-    # Iterate through ht
-    for j in ht:
+        value = (i, difference)
+        # Store value from ht as variable `ht_val`
         ht_val = hash_table_retrieve(ht, weight)
-        # Compare j to limit - j
-        if j == (limit - j):
-            # Retrieve ht_value
-            return ht_val
-        else:
-            pass
+
+        print(f"weight: {weight}, difference: {difference}, value: {value}")
+
+        # if weight is NOT equal to ht_val[1]
+        if weight != ht_val[1]:
+            hash_table_insert(ht, weight, value)
+        # If weight is equal to ht_val[1]:
+        elif weight == ht_val[1]:
+            # return (weight, ht_val[0])
+            return (weight, ht_val[0])
+
+        # # If weight not in ht:
+        # if weight not in ht:
+        #     # Add key:weight + value:(its_index, difference) to ht
+        #     hash_table_insert(ht, weight, i)
+        # # If weight IS in ht:
+        # else:
+        #     pass
+
+    # # Iterate through ht
+    # for j in ht:
+    #     ht_val = hash_table_retrieve(ht, weight)
+    #     # Compare j to limit - j
+    #     if j == (limit - j):
+    #         # Retrieve ht_value
+    #         return ht_val
+    #     else:
+    #         pass
 
     return None
 
