49f400b49b5d7fe0e27c6e354da73e2e9bc4e358
tania <tanislavens@yahoo.com>
2020-02-14 11:22:14 -0500

    added code to hashtables ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,15 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(length):
+        if tickets[i].source == "NONE":
+            route[0] = tickets[i].destination
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
+    
+    for j in range(length):
+        if route[j-1] is not None:
+            route[j] = hash_table_retrieve(hashtable, route[j-1])
+    
+    return route[:-1]
 
     return route
